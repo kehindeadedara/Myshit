c5cba1278e8c0c9ff508f251e04936e6e5ec216f
kehindeadedara <35585101+kehindeadedara@users.noreply.github.com>
2021-03-24 17:02:20 -0400

    Add files via upload

---
+++ get_threshold.py
@@ -0,0 +1,16 @@
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1015(i2c)
+ads.gain = 2/3
+
+# Create single-ended input on channel 0
+chan = AnalogIn(ads, ADS.P0)
+
+
+print("current threshold is {}".format(chan.value))
+print("current voltage is {}".format(chan.voltage))
