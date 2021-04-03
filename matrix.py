d36c2d5e63c1b47c2bceea7ee15497d71179f1fc
Kehinde Adedara <kehindeadedara2000@gmail.com>
2021-04-02 20:49:33 -0400

    vlc_car update

--- matrix.py
+++
@@ -1,9 +0,0 @@
-import time
-import busio
-import board
-import Jetson.GPIO as GPIO
-from adafruit_ht16k33 import matrix
-from Myshit.MatrixLED.BackMatrix import LEDmatrix
-
-frontled = LEDmatrix(0x71, 0x72)	
-frontled.transfer_bit(0.3)
