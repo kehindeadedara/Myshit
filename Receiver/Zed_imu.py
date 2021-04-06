d441b6401ca7316e28d02a7decb34eed12c37bcf
Kehinde Adedara <35585101+kehindeadedara@users.noreply.github.com>
2021-04-06 15:04:43 -0400

    Update Zed_imu.py

--- Receiver/Zed_imu.py
+++ Receiver/Zed_imu.py
@@ -1,1 +1,3 @@
-
+https://github.com/stereolabs/zed-examples/blob/master/tutorials/tutorial%207%20-%20sensor%20data/python/sensor_data.py
+  
+https://github.com/stereolabs/zed-examples/tree/master/tutorials/tutorial%207%20-%20sensor%20data/python
